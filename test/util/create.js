5a13c8a4a40b0fe3d528c15fb3beeadbd4eeaae1
gaurav563 <563gauravshakya@gmail.com>
2020-08-09 18:13:57 +0530

    changes

--- test/util/create.js
+++ test/util/create.js
@@ -1,7 +1,7 @@
 const tmp = require('tmp-promise')
-const dht = require('@hyperswarm/dht')
+const dht = require('dwswarm-dht')
 
-const { HyperdriveClient } = require('hyperdrive-daemon-client')
+const { HyperdriveClient } = require('dwebfs-daemon-client')
 const HyperdriveDaemon = require('../..')
 
 const BASE_PORT = 4101
